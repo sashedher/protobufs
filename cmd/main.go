11751b10960d810e584acf65c58c21bad3dd467d
Sashedher J <sashedherj@SZc.local>
2025-08-24 19:30:29 +0530

    hello world

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sashedher/protobufs/protos"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	protos.HelloProtos()
+}
---
+++ protos/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hello_protos() {
+	fmt.Println("Hello, Protobufs!")
+}
